93b55db88db005fc88709fc47c66172eaa30b97b
GG-II <105560586+GG-II@users.noreply.github.com>
2025-03-08 15:24:20 -0600

    Calculo del Perimetro del Trapecio

---
+++ src/main/java/com/mycompany/parcialuno/PerimetroTrapecio.java
@@ -0,0 +1,20 @@
+/**
+ * Autor: Gerbert García
+ * Clase que calcula el area de un trapecio
+ */
+package com.mycompany.parcialuno;
+
+public class PerimetroTrapecio {
+    
+    /**
+     * Método que calcula el perimetro de un trapecio.
+     * @param l1 Lado 1
+     * @param l2 Lado 2
+     * @param l3 Lado 3
+     * @param l4 Lado 4
+     * @return Resultado del calculo
+     */
+    public int perimetroTrap(int l1, int l2, int l3, int l4) {
+        return l1 + l2 + l3 + l4;
+    }
+}
